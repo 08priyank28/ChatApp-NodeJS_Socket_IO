d069e68bbe490f00192357c326ca7384a11bada5
08priyank28 <08priyank28@gmail.com>
2023-05-14 20:17:42 +0530

    ChatApp Completed

---
+++ chat/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Join from "./component/Join/Join";
+import Chat from "./component/Chat/Chat"
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" Component={Join} />
+          <Route exact path="/chat" Component={Chat} />
+        </Routes>
+      </Router>
+
+    </div >
+  );
+}
+
+export default App;
